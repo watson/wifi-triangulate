549c16f19f9af234d0242e151e1f4c8ddaebdc2c
Matias Singers <mail@matiassingers.com>
2015-06-15 18:03:40 +0800

    Create CLI

---
+++ cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+'use strict';
+
+var pkg = require('./package.json');
+var wifiTriangulate = require('./');
+var argv = process.argv.slice(2);
+
+function help() {
+  console.log([
+    '',
+    '  ' + pkg.description,
+    '',
+    '  Example',
+    '    wifi-triangulate',
+    '    => {',
+    '         "lat": 38.0690894,',
+    '         "lng": -122.8069356,',
+    '         "accuracy": 42',
+    '       }'
+  ].join('\n'));
+}
+
+if (argv.indexOf('--help') !== -1) {
+  help();
+  return;
+}
+
+if (argv.indexOf('--version') !== -1) {
+  console.log(pkg.version);
+  return;
+}
+
+wifiTriangulate(function(error, location) {
+  console.log(JSON.stringify(location, null, 4));
+});
